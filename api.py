f4d5eb7d6ea22bd7c2518a4c7edead55c15e58bf
gustavofhabermann <160959136+gustavofhabermann@users.noreply.github.com>
2025-03-20 19:40:37 -0300

    Update api.py Rick and Mort

--- api.py
+++ api.py
@@ -32,8 +32,12 @@ class API_Rick_Morty(API_consumer):
         return self.__URL
 
     def extract(self, id):
-    # Atividade 3, que resultará o segundo commit
-        pass
+        URL = self.URL + str(id)
+        try:
+            dado = requests.get(URL).json()
+            return (dado.get('id'), dado.get('name'), dado.get('species'))
+        except:    
+            pass
 
 class API_Star_Wars(API_consumer):
     ''' The universe of Star Wars '''
